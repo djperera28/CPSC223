f3a70ab71d404b874a1cba4a4029106f23c13132
djperera28 <143140944+djperera28@users.noreply.github.com>
2026-09-03 16:02:04 +0000

    getting catchphrase input from the user and printing out with uppercase, lowercase, and removing left or right spaces

---
+++ M1A4.py
@@ -0,0 +1,10 @@
+## Name: David Perera
+# Student ID: 884532367
+# Section: 07 
+# Assignment: Module 1 Assignment 4
+
+catchphrase_str = input("What is your catchphrase? ")
+print(f"[{catchphrase_str.upper()}]")
+print(f"[{catchphrase_str.lower()}]")
+print(f"[{catchphrase_str.lstrip()}]")
+print(f"[{catchphrase_str.rstrip()}]")
