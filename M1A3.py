65eafd47b6fa24584cf8431a7dcea898025f6754
djperera28 <143140944+djperera28@users.noreply.github.com>
2026-09-03 15:34:39 +0000

    Finished M1A3 by having the output print out the correct arithmetic

---
+++ M1A3.py
@@ -0,0 +1,12 @@
+## Name: David Perera
+# Student ID: 884532367
+# Section: 07 
+# Assignment: Module 1 Assignment 3
+
+first_int = int(input("What is the integer? "))
+second_float = float(input("What is the float? "))
+
+print(f"{first_int} + {second_float} is {first_int + second_float}")
+print(f"{first_int} - {second_float} is {first_int - second_float}")
+print(f"{first_int} * {second_float} is {first_int * second_float}")
+print(f"{first_int} / {second_float} is {first_int / second_float}")
