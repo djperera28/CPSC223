93c732bcbbcafc83c9bb75a75d12a15ee4ce9e78
djperera28 <143140944+djperera28@users.noreply.github.com>
2026-09-10 19:58:57 +0000

    Forgot to remove un-needed print statements

--- M2A3.py
+++ M2A3.py
@@ -4,7 +4,6 @@
 # Assignment: Module 2 Assignment 3
 
 uber_list = list(range(100,201,2))
-print(uber_list)
 
 start = int(input("What is the start of your slice? "))
 end = int(input("What is the end of your slice? "))
@@ -14,5 +13,4 @@ data_list = uber_list[start:end]
 total_int = 0
 for avg in data_list:
     total_int += avg
-print(data_list)
 print(f"Your slice contains {len(data_list)} values and has an average value of {total_int / len(data_list)}")
