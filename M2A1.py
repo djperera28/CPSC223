d3b0b491269c8ec07b0e4055e3c29a7d1469b246
djperera28 <143140944+djperera28@users.noreply.github.com>
2026-09-10 15:59:10 +0000

    Finished Module 2 Assignment 1

---
+++ M2A1.py
@@ -0,0 +1,12 @@
+## Name: David Perera
+# Student ID: 884532367
+# Section: 07 
+# Assignment: Module 2 Assignment 1
+
+g_list = []
+g_list.append(input("What is your favorite game? "))
+g_list.append(input("What is your second favorite game? "))
+g_list.append(input("What is your third favorite game? "))
+print(f"One of your favorite games is {g_list.pop().title()}")
+print(f"One of your favorite games is {g_list.pop().title()}")
+print(f"One of your favorite games is {g_list.pop().title()}")
